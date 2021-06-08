190e5e8623cbe479cd3a10abfe2185004e082e89
Benis Tambe <will.jenifer@yahoo.com>
2021-06-08 06:25:38 -0400

    commit tree 191e4b278d11eafe2bc3950a236dc071beff0f05

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.secret_token = SecureRandom.hex(64)
 end
